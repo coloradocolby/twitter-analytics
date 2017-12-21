678460e7084b1fcd1b21a2c60fd2d963a2e2e37d
coloradocolby <coloradocolby@gmail.com>
2017-12-21 01:12:15 -0700

    remove localhost

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -57,7 +57,7 @@ class Dashboard extends Component{
     selectedTime ? this.setState( () => ({ selectedTime: selectedTime.value })) : this.setState(() => ({ selectedTime: '' })); 
   }
   componentDidMount() {
-    axios.post('http://localhost:8000/api/user', {
+    axios.post('/api/user', {
       accessToken: this.state.credential.accessToken,
       secret: this.state.credential.secret,
       uid: this.state.user.providerData[0].uid
@@ -78,7 +78,7 @@ class Dashboard extends Component{
         favourites_count
       }));
       // GET USER KLOUT SCORE
-      axios.post('http://localhost:8000/api/klout', {
+      axios.post('/api/klout', {
         screenName: this.state.screen_name
       }).then((resp) => {
         const klout_score = resp.data.score;
@@ -96,7 +96,7 @@ class Dashboard extends Component{
   handleTweet = () => {
 
     const { credential, tweet } = this.state;
-    axios.post('http://localhost:8000/api/tweet', {
+    axios.post('/api/tweet', {
       accessToken: credential.accessToken,
       secret: credential.secret,
       status: tweet
